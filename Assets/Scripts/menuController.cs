066307dda5ae0cda6170628d4b672bf990d26739
rominaromani <mariajulia.romani@utec.edu.pe>
2024-06-30 12:46:29 -0500

    Avance de menu

--- Assets/Scripts/menuController.cs
+++ Assets/Scripts/menuController.cs
@@ -1,14 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class menuController : MonoBehaviour
+
 {
-    public void StartBtn()
+    void Start()
     {
 
-    SceneManager.LoadScene("ScapeRoom"); 
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void StartBtn(string SceneName)
+    {
+
+    SceneManager.LoadScene(SceneName); 
     }
 
 }
